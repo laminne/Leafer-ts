bf70e32d6c9175d8bc8b4695f1cd9d67e8146f16
Tatsuto Laminne Yamamoto <laminne33569@approvers.dev>
2021-05-05 15:06:18 +0900

    脆弱性対策

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Client, MessageAttachment } from "discord.js"
-import {prisma, reg, allusers, getall, discord_id, gh_user_name, img_update, register, update, get, screenshot } from "./function"
+import {prisma, reg, allusers, getall, img_update, register, update, get, screenshot } from "./function"
 
 const client = new Client()
 const env:any = process.env
@@ -12,13 +12,15 @@ client.on('ready', () => {
 
 
 client.on('message', async (message:any) =>{
+    const gh_not_user_name_urls = ["features", "mobile", "sponsors", "customer-stories", "team", "enterprise", "explore", "topics", "collections", "events", "readme", "marketplace", "pricing", "login", "join", "site-map", "git-guides", "about"]
+
     if (message.author.bot) {
         return
     }
     
     if (message.content === "l!get") {
     	let grass:any
-    	
+
         message.channel.send(`<@${message.author.id}>\n取得を開始します,これには時間がかかります`)
         grass = JSON.parse(await get(message.author.id))
         
@@ -27,7 +29,6 @@ client.on('message', async (message:any) =>{
                 message.channel.send(`<@${message.author.id}>\nエラーが発生しました:\n` + "```"+ e + "```")
                 throw e
             })
-        message.channel.send(`<@${message.author.id}>\n取得しました`)
     }
 
     if (message.content === "l!help") {
@@ -56,23 +57,35 @@ client.on('message', async (message:any) =>{
     	let gh_user_name:string
     	let discord_id:string
     	
-        gh_user_name = message.content.substr(9, message.content.length)
-        discord_id = message.author.id
-        console.log(discord_id, gh_user_name)
-        
-        let status:string = "成功"
-        
-        update(discord_id,gh_user_name)
-            .catch(e => {
-                message.channel.send(`<@${message.author.id}>\n` + "```"+ e + "```")
-                status = "失敗"
-                throw e
-            })
-            .finally(async ()=> {
-                await prisma.$disconnect()
-                
-                await message.channel.send(`<@${message.author.id}>\n` + "再登録に" + status + "しました")
-            })
+        gh_user_name = message.content.substr(9, message.content.length).trim()
+        if (gh_user_name.match(/\//)){
+            console.log("不正")
+            message.channel.send(`<@${message.author.id}>\n` + "不正なユーザー名です")
+            return
+        }
+
+        if (gh_not_user_name_urls.includes(gh_user_name)){
+            console.log("GitHub")
+            message.channel.send(`<@${message.author.id}>\nそのユーザー名は不正です`)
+            return
+        } else {
+            discord_id = message.author.id
+            console.log(discord_id, gh_user_name)
+
+            let status:string = "成功"
+
+            update(discord_id,gh_user_name)
+                .catch(e => {
+                    message.channel.send(`<@${message.author.id}>\n` + "```"+ e + "```")
+                    status = "失敗"
+                    throw e
+                })
+                .finally(async ()=> {
+                    await prisma.$disconnect()
+
+                    await message.channel.send(`<@${message.author.id}>\n` + "再登録に" + status + "しました")
+                })
+        }
     }
 
     
@@ -80,27 +93,39 @@ client.on('message', async (message:any) =>{
     	let gh_user_name:string
     	let discord_id:string
     	    	
-        gh_user_name = message.content.substr(11, message.content.length)
+        gh_user_name = message.content.substr(11, message.content.length).trim()
+        if (gh_user_name.match(/\//)){
+            console.log("不正")
+            message.channel.send(`<@${message.author.id}>\n` + "不正なユーザー名です")
+            return
+        }
+
         discord_id = message.author.id
-        let status:string = "成功"
-        register()
-            .catch(e => {
-                message.channel.send(`<@${message.author.id}>\n` + "```"+ e + "```")
-                status = "失敗"
-                throw e
-            })
-            .finally(async ()=> {
-                await prisma.$disconnect()
-                console.log(`${reg}`)
-                await message.channel.send(`<@${message.author.id}>\n` + "登録に" + status + "しました")
-            })
+        if (gh_not_user_name_urls.includes(gh_user_name)){
+            console.log("不正なURL:GitHub")
+            message.channel.send(`<@${message.author.id}>\nそのユーザー名は不正です`)
+            return
+        } else {
+            let status:string = "成功"
+            register()
+                .catch(e => {
+                    message.channel.send(`<@${message.author.id}>\n` + "```"+ e + "```")
+                    status = "失敗"
+                    throw e
+                })
+                .finally(async ()=> {
+                    await prisma.$disconnect()
+                    console.log(`${reg}`)
+                    await message.channel.send(`<@${message.author.id}>\n` + "登録に" + status + "しました")
+                })
+        }
     }
 
 
     if (message.content === "草" || message.content === "kusa" || message.content === "grass" || message.content === "<:grass:720993758832754728>") {
     	let grass:any
     	
-        await get(message.author.id)
+        grass = await get(message.author.id)
             .catch(e => {
                 message.channel.send(`<@${message.author.id}>\n` + "```"+ e + "```")
                 throw e
